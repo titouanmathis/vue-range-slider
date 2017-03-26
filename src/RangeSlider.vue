a48045577400b9c7c369723f74beae143e33f5a3
Titouan Mathis <titouan.mathis@gmail.com>
2017-03-26 17:17:00 +0200

    Add basic functionnality

--- src/RangeSlider.vue
+++ src/RangeSlider.vue
@@ -1,33 +1,14 @@
 <template>
-	<div>
-
-	<p>
-		<label for="min">MIN => <span v-html="MIN"></span></label>
-		<br>
-		<input id="min" type="range" :min="min" :max="MAX" :step="step" :value="value[0]" v-model="MIN" @input="emitValue">
-	</p>
-
-	<p>
-		<label for="max">MAX => <span v-html="MAX"></span></label>
-		<br>
-		<input id="max" type="range" :min="MIN" :max="max" :step="step" :value="value[1]" v-model="MAX" @input="emitValue">
-	</p>
-
-	<p>&nbsp;</p>
-
-	<p>
-		<div class="range-slider" :class="`range-slider--${direction}`">
-			<div class="range-slider__track"></div>
-			<div class="range-slider__control range-slider__control--min">
-				<span class="range-slider__tooltip" v-html="formatValue(MIN)"></span>
-			</div>
-			<div class="range-slider__control range-slider__control--max">
-				<span class="range-slider__tooltip" v-html="formatValue(MAX)"></span>
-			</div>
+	<div class="range-slider" :class="`range-slider--${direction}`">
+		<div class="range-slider__track">
+			<div class="range-slider__filled"></div>
 		</div>
-
-	</p>
-
+		<div class="range-slider__control range-slider__control--min" @mousedown="mousedownHandler">
+			<span class="range-slider__tooltip range-slider__tooltip--top" v-html="formatValue(CONTROL_MIN.VALUE)"></span>
+		</div>
+		<div class="range-slider__control range-slider__control--max" @mousedown="mousedownHandler">
+			<span class="range-slider__tooltip range-slider__tooltip--bottom" v-html="formatValue(CONTROL_MAX.VALUE)"></span>
+		</div>
 	</div>
 </template>
 
@@ -51,7 +32,7 @@
 			},
 			step: {
 				type: Number,
-				default: 10
+				default: 1
 			},
 			formatter: {
 				type: String,
@@ -81,17 +62,21 @@
 				POINTER_X: 0,
 				POINTER_Y: 0,
 				X_MIN: 0,
-				X_MAX: 0,
-				X: 0,
+				X_MAX: 100,
 				Y_MIN: 0,
-				Y_MAX: 0,
-				Y: 0,
+				Y_MAX: 100,
+				CONTROL_MIN: {
+					X: 0,
+					Y: 0,
+					VALUE: this.value[0]
+				},
+				CONTROL_MAX: {
+					X: 0,
+					Y: 0,
+					VALUE: this.value[1]
+				},
 				SLIDER_WIDTH: 0,
 				SLIDER_HEIGHT: 0,
-				EVENTS: [
-					'mousedown',
-					'touchstart',
-				],
 				IS_DESTROYED: true,
 				$currentTarget: null
 			}
@@ -105,7 +90,7 @@
 			 *
 			 */
 			emitValue() {
-				this.$emit('input', [Number(this.MIN), Number(this.MAX)]);
+				this.$emit('input', [Number(this.CONTROL_MIN.VALUE), Number(this.CONTROL_MAX.VALUE)]);
 			},
 
 
@@ -136,6 +121,7 @@
 
 
 			saveSizes() {
+				console.log('saveSizes');
 				const SIZES = this.$slider.getBoundingClientRect()
 
 				this.SLIDER_WIDTH = SIZES.width
@@ -148,44 +134,39 @@
 				this.Y_MAX = SIZES.top + SIZES.height
 			},
 
+			start($target, POINTER_X, POINTER_Y) {
+				this.updatePointerPosition(POINTER_X, POINTER_Y);
+				this.X = this.getX($target.__TYPE)
+				this.Y = this.getY($target.__TYPE)
+				this.$currentTarget = $target
+				this.IS_LOOPING = true
+			},
+
+			end() {
+				this.$currentTarget = null
+				this.IS_LOOPING = false
+				this.emitValue()
+			},
+
 			resizeHandler(e) {
 				this.saveSizes()
 			},
 
 			mousedownHandler({ target, clientX , clientY }) {
-				this.updatePointerPosition(clientX, clientY);
 				document.addEventListener('mousemove', this);
 				document.addEventListener('mouseup', this);
-				this.X = this.getX()
-				this.Y = this.getY()
-				this.$currentTarget = target
-				this.IS_LOOPING = true
+				this.start(target, clientX, clientY);
 			},
 
 
 			mouseupHandler(e) {
 				document.removeEventListener('mousemove', this);
 				document.removeEventListener('mouseup', this);
-				this.$currentTarget = null
-				this.IS_LOOPING = false
+				this.end()
 			},
-
 
 			mousemoveHandler({ clientX , clientY }) {
 				this.updatePointerPosition(clientX, clientY);
-			},
-
-			touchstartHandler(e) {
-				this.$doc.addEventListener('touchmove', this)
-			},
-
-			touchendHandler(e) {
-				this.$doc.removeEventListener('touchmove', this)
-			},
-
-			touchmoveHandler({ touches }) {
-				const touch = touches[0]
-				this.updatePointerPosition(touch.clientX, touch.clientY)
 			},
 
 			updatePointerPosition(X, Y) {
@@ -194,60 +175,110 @@
 			},
 
 
-			getX() {
-				return this.POINTER_X < this.X_MIN ? 0 : this.POINTER_X > this.X_MAX ? this.SLIDER_WIDTH : this.POINTER_X - this.X_MIN
+			getX(TYPE) {
+				const X = this.POINTER_X < this.X_MIN ? 0 : this.POINTER_X > this.X_MAX ? this.SLIDER_WIDTH : this.POINTER_X - this.X_MIN
+
+				if (TYPE === 'CONTROL_MIN') {
+					return X > this.CONTROL_MAX.X ? this.CONTROL_MAX.X : X
+				} else {
+					return X < this.CONTROL_MIN.X ? this.CONTROL_MIN.X : X
+				}
 			},
 
-			getY() {
-				return this.POINTER_Y < this.Y_MIN ? 0 : this.POINTER_Y > this.Y_MAX ? this.SLIDER_HEIGHT : this.POINTER_Y - this.Y_MIN
+			getY(TYPE) {
+				const Y = this.POINTER_Y < this.Y_MIN ? 0 : this.POINTER_Y > this.Y_MAX ? this.SLIDER_HEIGHT : this.POINTER_Y - this.Y_MIN
+
+				if (TYPE === 'CONTROL_MIN') {
+					return Y > this.CONTROL_MAX.Y ? this.CONTROL_MAX.Y : Y
+				} else {
+					return Y < this.CONTROL_MIN.Y ? this.CONTROL_MIN.Y : Y
+				}
 			},
 
 			loop() {
 				if (this.IS_DESTROYED || !this.IS_LOOPING) return false
 
-				// Horizontal layout
-				if (this.$currentTarget !== null && this.direction === 'horizontal') {
-					const X = this.getX()
-					this.X += (X - this.X) * this.EASE
-					this.$currentTarget.style.transform = `translate(${this.X}px, 0)`
-				}
+				if (this.$currentTarget !== null) {
+					const TYPE = this.$currentTarget.__TYPE
 
-				// Vertical layout
-				if (this.$currentTarget !== null && this.direction === 'vertical') {
-					const Y = this.getY()
-					this.Y += (Y - this.Y) * this.EASE
-					this.$currentTarget.style.transform = `translate(0, ${this.Y}px)`
+					// Horizontal layout
+					if (this.direction === 'horizontal') {
+						const X = this.getX(TYPE)
+						this[TYPE].X += (X - this[TYPE].X) * this.EASE
+						this.$currentTarget.style.transform = `translate3d(${this[TYPE].X}px, 0, 0)`
+						this.$filled.style.left = `${this.CONTROL_MIN.X}px`
+						this.$filled.style.right = `${this.SLIDER_WIDTH - this.CONTROL_MAX.X}px`
+
+						this[TYPE].VALUE = Math.round((this[TYPE].X * this.max / this.SLIDER_WIDTH) / this.step) * this.step
+					}
+
+					// Vertical layout
+					if (this.direction === 'vertical') {
+						const Y = this.getY(TYPE)
+						this[TYPE].Y += (Y - this[TYPE].Y) * this.EASE
+						this.$currentTarget.style.transform = `translate3d(0, ${this[TYPE].Y}px, 0)`
+						this.$filled.style.top = `${this.CONTROL_MIN.Y}px`
+						this.$filled.style.bottom = `${this.SLIDER_HEIGHT - this.CONTROL_MAX.Y}px`
+
+						this[TYPE].VALUE = Math.round((this[TYPE].Y * this.max / this.SLIDER_HEIGHT) / this.step) * this.step
+					}
+
+					this.emitValue()
 				}
 				requestAnimationFrame(this.loop.bind(this))
+			},
+
+			initPositions() {
+				this.CONTROL_MIN.X = this.value[0] * this.SLIDER_WIDTH / this.max
+				this.CONTROL_MIN.Y = this.value[0] * this.SLIDER_HEIGHT / this.max
+				this.CONTROL_MAX.X = this.value[1] * this.SLIDER_WIDTH / this.max
+				this.CONTROL_MAX.Y = this.value[1] * this.SLIDER_HEIGHT / this.max
+
+				if (this.direction === 'horizontal') {
+					this.$filled.style.left = `${this.CONTROL_MIN.X}px`
+					this.$filled.style.right = `${this.SLIDER_WIDTH - this.CONTROL_MAX.X}px`
+					this.CONTROL_MIN.VALUE = Math.round((this.CONTROL_MIN.X * this.max / this.SLIDER_WIDTH) / this.step) * this.step
+					this.CONTROL_MAX.VALUE = Math.round((this.CONTROL_MAX.X * this.max / this.SLIDER_WIDTH) / this.step) * this.step
+				} else {
+					this.$filled.style.top = `${this.CONTROL_MIN.Y}px`
+					this.$filled.style.bottom = `${this.SLIDER_HEIGHT - this.CONTROL_MAX.Y}px`
+					this.CONTROL_MIN.VALUE = Math.round((this.CONTROL_MIN.Y * this.max / this.SLIDER_HEIGHT) / this.step) * this.step
+					this.CONTROL_MAX.VALUE = Math.round((this.CONTROL_MAX.Y * this.max / this.SLIDER_HEIGHT) / this.step) * this.step
+				}
+
+				forEach(this.$controls, ($control) => {
+					const TYPE = $control.__TYPE;
+					if (this.direction === 'horizontal') {
+						$control.style.transform = `translate3d(${this[TYPE].X}px, 0, 0)`
+					} else {
+						$control.style.transform = `translate3d(0, ${this[TYPE].Y}px, 0)`
+					}
+				})
 			}
 		},
 		watch: {
-			POINTER(newValue) {
-				console.log(newValue);
-			},
-			IS_LOOPING(yes) {
-				if (yes) this.loop()
+			IS_LOOPING(newValue) {
+				if (newValue) this.loop()
 			}
 		},
 		mounted() {
 			this.IS_DESTROYED = false
-			this.$slider = this.$el.querySelector('.range-slider')
-			this.$controls = this.$el.querySelectorAll('.range-slider__control');
 
-			forEach(this.$controls, ($control, index) => {
-				forEach(this.EVENTS, (EVENT) => {
-					$control.addEventListener(EVENT, this)
-				})
-			})
+			this.$slider = this.$el
+			this.$filled = this.$el.querySelector('.range-slider__filled')
+
+			this.$controls = this.$el.querySelectorAll('.range-slider__control');
+			this.$controls[0].__TYPE = 'CONTROL_MIN'
+			this.$controls[1].__TYPE = 'CONTROL_MAX'
+
 			this.saveSizes();
+			this.initPositions()
+
+			window.addEventListener('resize', this)
 		},
 		beforeDestroy() {
 			this.IS_DESTROYED = true
-			forEach(this.$controls, ($control, index) => {
-				forEach(this.EVENTS, (EVENT) => {
-					$control.removeEventListener(EVENT, this)
-				})
-			})
+			window.removeEventListener('resize', this)
 		}
 	}
 </script>
@@ -277,6 +308,22 @@
 		width: 100%;
 		height: 100%;
 		background: #333;
+		overflow: hidden;
+	}
+
+	.range-slider__filled {
+		position: absolute;
+		background: red;
+		pointer-events: none;
+		will-change: left, right;
+
+		.range-slider--horizontal & {
+			height: 100%;
+		}
+
+		.range-slider--vertical & {
+			width: 100%;
+		}
 	}
 
 	.range-slider__control {
@@ -314,7 +361,6 @@
 		pointer-events: none;
 
 		.range-slider--horizontal & {
-			bottom: 100%;
 			left: 50%;
 			text-align: center;
 			transform: translate(-50%, 0);
@@ -322,8 +368,31 @@
 
 		.range-slider--vertical & {
 			top: 50%;
-			left: 100%;
 			transform: translate(0, -50%);
+		}
+	}
+
+	.range-slider__tooltip--top {
+
+		.range-slider--horizontal & {
+			bottom: 100%;
+		}
+
+		.range-slider--vertical & {
+			left: 100%;
+			text-align: left;
+		}
+	}
+
+	.range-slider__tooltip--bottom {
+
+		.range-slider--horizontal & {
+			top: 100%;
+		}
+
+		.range-slider--vertical & {
+			right: 100%;
+			text-align: right;
 		}
 	}
 </style>
