605b4435168a6b6889af95daf4fb7a18a4b27c98
Titouan Mathis <titouan.mathis@gmail.com>
2017-03-28 08:45:54 +0200

    Extract the component in its own files on build

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 var path = require('path')
 var webpack = require('webpack')
+var utils = require('./utils')
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: '[name].js',
+    chunkFilename: '[id].js'
   },
   module: {
     rules: [
@@ -14,16 +17,10 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {
-            
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-            
-          }
-          // other vue-loader options go here
+          loaders: utils.cssLoaders({
+				    sourceMap: true,
+				    extract: true
+				  })
         }
       },
       {
@@ -72,6 +69,21 @@ if (process.env.NODE_ENV === 'production') {
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
+    }),
+    // extract css into its own file
+    new ExtractTextPlugin({
+      filename: '[name].css'
+    }),
+    // split vendor js into its own file
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'range-slider',
+      minChunks: function (module, count) {
+        // any required modules inside node_modules are extracted to vendor
+        return (
+          module.resource &&
+          /\.vue$/.test(module.resource)
+        )
+      }
     })
   ])
 }
